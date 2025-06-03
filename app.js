25abd7b225283638b24110054a0125096c6f4fd0
Kharpreet571 <kourharpreet571@gmail.com>
2025-06-04 09:36:13 +1200

    set view directory

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require('path'); // ✅ Make sure this is included
 
 const app = express();
 
-app.set('view engine', 'ejs');
+const path = require('path');
+
+// Set views directory
 app.set('views', path.join(__dirname, 'views'));
-
-
+app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 
