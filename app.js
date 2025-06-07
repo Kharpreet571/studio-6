07a035ca743416329c6c2382edfa72a302867fca
Kharpreet571 <kourharpreet571@gmail.com>
2025-06-07 22:27:40 +1200

    add  guidance

--- app.js
+++ app.js
@@ -22,55 +22,19 @@ app.get('/home', (req, res) => {
 app.get('/introduction', (req, res) => {
   res.render('introduction', { currentPage: 'introduction' });
 });
+app.get('/guidance', (req, res) => {
+  res.render('guidance'); // assumes there's a guidance.ejs in the views folder
+});
+
 
 // For the home route
 app.get('/', (req, res) => {
   res.render('home', { activePage: 'home' });
 });
 
-// For the challenges route
-app.get('/challenges', (req, res) => {
-  res.render('challenges', { activePage: 'challenges' });
-});
 
 app.get('/privacy-act', function (req, res) {
     res.render('privacy-act', { title: 'Privacy Act 2020' });
-});
-
-app.get('/principle-5', function (req, res) {
-    res.render('principle-5', { title: 'Principle 5' });
-});
-
-app.get('/principle-6', function (req, res) {
-    res.render('principle-6', { title: 'Principle 6' });
-});
-
-app.get('/principle-9', function (req, res) {
-    res.render('principle-9', { title: 'Principle 9' });
-});
-
-app.get('/principle-11', function (req, res) {
-    res.render('principle-11', { title: 'Principle 11' });
-});
-
-app.get('/gdpr', function (req, res) {
-    res.render('gdpr', { title: 'GDPR' });
-});
-
-app.get('/health-data', function (req, res) {
-    res.render('health-data', { title: 'Health Data' });
-});
-
-app.get('/nz-privacy-law', function (req, res) {
-    res.render('nz-privacy-law', { title: 'NZ Privacy Law' });
-});
-
-app.get('/conclusion', function (req, res) {
-    res.render('conclusion', { title: 'Conclusion' });
-});
-
-app.get('/privacy', (req, res) => {
-    res.render('privacy-act');
 });
 
 
