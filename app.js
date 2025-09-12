22c8a420d6f6601ff596c7835acb069f9eb1f90b
Kharpreet571 <kourharpreet571@gmail.com>
2025-09-12 13:32:34 +1200

    add new font size

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.set("view engine", "ejs");
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from public folder
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public')); // or wherever your CSS files are located
 
-app.get("/", (req, res) => {
-  res.render("home");
+// Routes
+app.get('/', (req, res) => {
+  res.render('home'); // renders views/home.ejs
 });
 
-app.get("/", (req, res) => {
-  res.render("about");
+app.get('/about', (req, res) => {
+  res.render('about'); // renders views/about.ejs
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+app.get('/laws', (req, res) => {
+  res.render('laws');
 });
+
+app.get('/issues', (req, res) => {
+  res.render('issues');
+});
+
+app.get('/stakeholders', (req, res) => {
+  res.render('stakeholders');
+});
+
+app.get('/solutions', (req, res) => {
+  res.render('solutions');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
