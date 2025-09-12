dfa4c1b0a16bf3fd2822e84243cb4cb7361468f2
Kharpreet571 <kourharpreet571@gmail.com>
2025-09-12 17:01:54 +1200

    Start the server

--- app.js
+++ app.js
@@ -1,41 +1,69 @@
+// app.js
 const express = require('express');
 const app = express();
 const path = require('path');
 
-// Set EJS as the view engine
+// ------------------------------
+// Set up view engine
+// ------------------------------
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files
+// ------------------------------
+// Serve static files from 'public' folder
+// ------------------------------
 app.use(express.static(path.join(__dirname, 'public')));
 
+// ------------------------------
 // Routes
+// ------------------------------
+
+// Home page
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+// About page
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Privacy Laws page
 app.get('/privacy-laws', (req, res) => {
     res.render('privacy-laws');
 });
 
+// Optional alias for /law URL
+app.get('/law', (req, res) => {
+    res.render('privacy-laws'); 
+});
+
+// Issue page
 app.get('/issue', (req, res) => {
     res.render('issue');
 });
 
+// Stakeholders page
 app.get('/stakeholders', (req, res) => {
     res.render('stakeholders');
 });
 
+// Solution page
 app.get('/solution', (req, res) => {
     res.render('solution');
 });
 
-// Start server
+// ------------------------------
+// Handle 404 errors
+// ------------------------------
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
+// ------------------------------
+// Start the server
+// ------------------------------
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
