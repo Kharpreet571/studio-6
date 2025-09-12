2afb8521bf16863bf28685a9de27ae7dc043f506
Kharpreet571 <kourharpreet571@gmail.com>
2025-09-12 16:07:40 +1200

    add port

--- app.js
+++ app.js
@@ -8,33 +8,34 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public')); // or wherever your CSS files are located
 
 // Routes
 app.get('/', (req, res) => {
-  res.render('home'); // renders views/home.ejs
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about'); // renders views/about.ejs
+    res.render('about');
 });
 
-app.get('/laws', (req, res) => {
-  res.render('laws');
+app.get('/law', (req, res) => {
+    res.render('law');
 });
 
-app.get('/issues', (req, res) => {
-  res.render('issues');
+app.get('/issue', (req, res) => {
+    res.render('issue');
 });
 
 app.get('/stakeholders', (req, res) => {
-  res.render('stakeholders');
+    res.render('stakeholders');
 });
 
-app.get('/solutions', (req, res) => {
-  res.render('solutions');
+app.get('/solution', (req, res) => {
+    res.render('solution');
 });
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
