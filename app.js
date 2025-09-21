25ad3f06acec8a1b57db270de98cc7e5cc8be987
Kharpreet571 <kourharpreet571@gmail.com>
2025-09-22 10:57:54 +1200

    Serve static files from 'public' folder remove

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// ------------------------------
-// Serve static files from 'public' folder
-// ------------------------------
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ------------------------------
